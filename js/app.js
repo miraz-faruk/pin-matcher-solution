d741d20869b0d9ce2622e902f025dc15eda3dd0f
mirazfaruk <mirazfaruk@gmail.com>
2024-01-27 00:58:30 +0600

    second commit

--- js/app.js
+++ js/app.js
@@ -2,10 +2,10 @@ function getPin() {
     const pin = generatePin();
     const pinString = pin + '';
 
-    if(pinString.length === 4){
+    if (pinString.length === 4) {
         return pin;
     }
-    else{
+    else {
         return getPin();
     }
 }
@@ -15,10 +15,32 @@ function generatePin() {
     return random;
 }
 
-document.getElementById('generate-pin').addEventListener('click', function(){
+document.getElementById('generate-pin').addEventListener('click', function () {
     const pin = getPin();
 
     //display pin
     const displayPinField = document.getElementById('display-pin');
     displayPinField.value = pin;
+})
+
+document.getElementById('calculator').addEventListener('click', function (event) {
+    const number = event.target.innerText;
+    const typedNumberField = document.getElementById('typed-numbers');
+    const previousTypedNumber = typedNumberField.value;
+    if (isNaN(number)) {
+        if (number === 'C') {
+            typedNumberField.value = '';
+        }
+        else if (number === '<') {
+            const digits = previousTypedNumber.split('');
+            digits.pop();
+            const remainingDigits = digits.join('');
+            typedNumberField.value = remainingDigits;
+        }
+    }
+    else {
+        const newTypedNumber = previousTypedNumber + number;
+        typedNumberField.value = newTypedNumber;
+    }
+    console.log();
 })
